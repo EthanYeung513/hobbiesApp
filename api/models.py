33276262dce78989204dc77bbf0db2ff7e450835
Ethan Yeung <ethanyeung513@outlook.com>
2024-12-02 17:36:51 +0000

    change models according to comments

--- api/models.py
+++ api/models.py
@@ -14,12 +14,13 @@ class AppUser(AbstractUser):
     date_of_birth = models.DateField()
 
 class Hobby(models.Model):
-    appUser = models.ManyToManyField(AppUser, through="AppUserDoesHobby")   
+    appUser = models.ManyToManyField(AppUser, through="AppUserHobby")   
     hobby_name = models.CharField(max_length=128)
 
-class AppUserDoesHobby(models.Model):
-    hobby_name = models.CharField(max_length=128)
+class AppUserHobby(models.Model):
     appUser = models.ForeignKey(AppUser, on_delete=models.CASCADE)
     hobby = models.ForeignKey(Hobby, on_delete=models.CASCADE)
+    date_started = models.DateField()
+    level_of_expertise = models.CharField(max_length=128)
 
 
