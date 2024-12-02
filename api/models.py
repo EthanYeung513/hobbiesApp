cf4d3845cc8d40d1c0caeb33d817495e97270a12
Ethan Yeung <ethanyeung513@outlook.com>
2024-12-02 15:57:26 +0000

    successfully create new models

--- api/models.py
+++ api/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
@@ -8,3 +9,11 @@ class PageView(models.Model):
 
     def __str__(self):
         return f"Page view count: {self.count}"
+
+
+class Hobby(models.Model):
+    hobby_name = models.CharField(max_length=128)
+
+class AppUser(AbstractUser):
+    date_of_birth = models.DateField()
+    hobbies = models.ManyToManyField(Hobby)
